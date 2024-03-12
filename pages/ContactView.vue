6cbcb353777b1a5f5df7a069ea5a3158730024fd
theolukayodeasemudara <blaqnativity@gmail.com>
2024-03-12 14:40:39 +0100

    still on form component

--- pages/ContactView.vue
+++ pages/ContactView.vue
@@ -1,39 +1,34 @@
 <template>
-  <section class="grid items-center justify-items-center p-6 shadow-md h-dvh">
+  <section class="grid items-center justify-items-center mb-4 h-dsh">
     <form
-      @submit.prevent
-      class="space-y-10 w-[100%] md:w-[50%] bg-white p-8 rounded-md shadow-md"
+      @submit.prevent="handleSubmit"
+      class="mt-10 space-y-10 w-[100%] md:w-[50%] bg-white p-8 rounded-md shadow-md"
     >
+      <div v-if="errorMessage" class="error">{{ errorMessage }}</div>
       <div class="title">
-        <p class="pText">Contact</p>
         <h1 class="headerText2 text-left md:text-start">Get in touch</h1>
       </div>
-      <div class="flex flex-col">
-        <label class="pText">Name:</label>
-        <input
-          type="text"
-          name="name"
-          placeholder="Enter first and last name"
-          required
-        />
-      </div>
+
       <div class="flex flex-col">
         <label class="pText">Email:</label>
-        <input
-          type="email"
-          placeholder="Enter a valid email"
-          name="email"
-          required
-        />
+        <input type="email" placeholder="enter valid email" v-model="email" />
+      </div>
+
+      <div class="grid gap-3">
+        <label class="pText">What kind of service do you require of me?</label>
+        <select v-model="role" class="w-[70%] p-3 outline-none">
+          <option value="Developer">Frontend Developer</option>
+          <option value="Wordpress">Wordpress Development</option>
+        </select>
       </div>
 
       <div class="flex flex-col">
         <label class="pText">Your message</label>
         <textarea
-          name="message"
-          placeholder="Enter your message"
+          v-model="message"
+          placeholder="Your message"
           cols="30"
-          rows="4"
+          rows="2"
         ></textarea>
       </div>
 
@@ -47,8 +42,23 @@
 
 <script>
 export default {
+  erroMessage: false,
   data() {
-    return {};
+    return {
+      name: "",
+      email: "",
+      message: "",
+      role: "",
+      errorMessage: "",
+    };
+  },
+  methods: {
+    handleSubmit() {
+      if (this.name === "" && this.message === "" && this.role === "") {
+        this.errorMessage =
+          "All input feilds are required to better communicate what you need my services for!!!";
+      }
+    },
   },
 };
 </script>
