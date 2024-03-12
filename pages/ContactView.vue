a7c8409b6acde346f9352b72c8fe909ddd2a10f4
theolukayodeasemudara <blaqnativity@gmail.com>
2024-03-12 12:50:02 +0100

    origin main

--- pages/ContactView.vue
+++ pages/ContactView.vue
@@ -2,7 +2,7 @@
   <section class="grid items-center justify-items-center p-6 shadow-md h-dvh">
     <form
       @submit.prevent
-      class="space-y-10 md:w-[50%] bg-white p-8 rounded-md shadow-md"
+      class="space-y-10 w-[100%] md:w-[50%] bg-white p-8 rounded-md shadow-md"
     >
       <div class="title">
         <p class="pText">Contact</p>
@@ -10,16 +10,31 @@
       </div>
       <div class="flex flex-col">
         <label class="pText">Name:</label>
-        <input type="text" />
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter first and last name"
+          required
+        />
       </div>
       <div class="flex flex-col">
         <label class="pText">Email:</label>
-        <input type="email" />
+        <input
+          type="email"
+          placeholder="Enter a valid email"
+          name="email"
+          required
+        />
       </div>
 
       <div class="flex flex-col">
         <label class="pText">Your message</label>
-        <textarea name="" id="" cols="30"></textarea>
+        <textarea
+          name="message"
+          placeholder="Enter your message"
+          cols="30"
+          rows="4"
+        ></textarea>
       </div>
 
       <button class="mainBtn flex items-center gap-2">
